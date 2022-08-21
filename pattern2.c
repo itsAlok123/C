dbfa2233bc84635ce7c5e673e73bbd8d9ae976e4
ALOK SAMAL <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:58:22 +0530

    Create pattern2.c

---
+++ pattern2.c
@@ -0,0 +1,19 @@
+# C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int i,j,a;
+  printf("\nEnter the value:");
+  scanf("%d",&a);
+  for(i=1;i<=a;i++)
+  {
+    printf("\n");
+    for(j=1;j<=i;j++)
+    {
+      printf(" * ");
+    }
+  }
+  return 0;
+}
