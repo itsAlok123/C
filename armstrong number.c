f823f9d6a8de25470e302e978524e42a34a4c3a6
ALOK SAMAL <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:56:37 +0530

    Create armstrong number.c

---
+++ armstrong number.c
@@ -0,0 +1,22 @@
+# C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n,r,sum=0,temp;
+printf("enter the number=");
+scanf("%d",&n);
+temp=n;
+while(n>0)
+{
+r=n%10;
+sum=sum+(r*r*r);
+n=n/10;
+}
+if(temp==sum)
+printf("armstrong  number ");
+else
+printf("not armstrong number");
+return 0;
+}
