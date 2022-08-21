b7931930168315412b44cd90e50b630b13812534
ALOK SAMAL <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:37:35 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,14 @@
+# C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter Two Numbers:");
+    scanf("%d" , &a);
+    scanf("%d" , &b);
+
+    printf("%d" , a+b);
+    return 0 ;
+}
