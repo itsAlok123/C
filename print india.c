71ea2a6ab8cc2d8fbaf9f3309864f423bfc774a0
ALOK SAMAL <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:50:24 +0530

    Create print india.c

---
+++ print india.c
@@ -0,0 +1,19 @@
+# C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n ;
+ i = 0;
+
+ printf("Number Of Times - ");
+ scanf("%d" , &n);
+
+ while(i<n )
+ {
+     printf("INDIA\n");
+     i ++ ;
+ }
+ return 0;
+}
