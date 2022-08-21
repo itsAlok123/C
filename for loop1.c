0bbe9f6ce919ce303ff3906dd81778ca244bcc8a
ALOK SAMAL <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:52:08 +0530

    Create for loop1.c

---
+++ for loop1.c
@@ -0,0 +1,15 @@
+# C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ int i,n,sum ;
+  printf("Enter the range \n");
+  scanf("%d" , &n );
+
+  for(i=0,sum=0;i<n;i++){
+    printf("%d\n",i);
+}
+ return 0;
+}
