ec5292f5ac43f63115f8a2b1bb0bec0186136ad5
ALOK SAMAL <111678044+itsAlok123@users.noreply.github.com>
2022-08-21 22:38:14 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,18 @@
+# C
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,temp;
+    printf("Enter the two numbers :-");
+    scanf("%d %d" , &a,&b);
+
+    a=temp;
+    a=b;
+    b=temp;
+
+    printf("%d %d" ,a,b);
+
+    return 0;
+}
